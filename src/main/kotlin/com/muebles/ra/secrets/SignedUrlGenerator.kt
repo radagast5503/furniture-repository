ff21227b651a48f1449f2b1238d055becbcf0a96
ferjjp <fer.jjp@gmail.com>
2021-08-28 17:35:27 -0300

    Add download url

--- src/main/kotlin/com/muebles/ra/secrets/SignedUrlGenerator.kt
+++ src/main/kotlin/com/muebles/ra/secrets/SignedUrlGenerator.kt
@@ -13,6 +13,7 @@ import javax.inject.Inject
 
 interface URLObtainer {
     fun uploadUrl(objectName: String?): Result<URL?>
+    fun downloadUrl(objectName: String?): Result<URL?>
 }
 
 @Configuration
@@ -20,34 +21,47 @@ open class SpringConfig {
     @Bean
     open fun urlObtainer(cfg: Config, keyObtainer: KeyObtainer): URLObtainer {
         val env = System.getenv("ENVIRONMENT") ?: "local"
-        if(!env.equals("local")) {
-            return com.muebles.ra.secrets.SignedUrlGenerator(cfg,keyObtainer)
+        if (!env.equals("local")) {
+            return com.muebles.ra.secrets.SignedUrlGenerator(cfg, keyObtainer)
         }
         return FakeUrlObtainer()
     }
 }
 
-class FakeUrlObtainer: URLObtainer {
+class FakeUrlObtainer : URLObtainer {
     override fun uploadUrl(objectName: String?): Result<URL?> {
-        return Result.success(URL("https://fake.com/itstrue"))
+        return Result.success(URL("https://fake.com/upload/$objectName"))
+    }
+
+    override fun downloadUrl(objectName: String?): Result<URL?> {
+        return Result.success(URL(("https://fake.com/download/$objectName")))
     }
 
 }
 
-class SignedUrlGenerator constructor(val cfg: Config, keyObtainer: KeyObtainer): URLObtainer {
+class SignedUrlGenerator constructor(val cfg: Config, keyObtainer: KeyObtainer) : URLObtainer {
 
     private val credentials = ServiceAccountCredentials.fromStream(ByteArrayInputStream(keyObtainer.getKey()))
 
-    private val storage = StorageOptions.newBuilder().setCredentials(credentials).setProjectId(cfg.projectName()).build().service
+    private val storage =
+        StorageOptions.newBuilder().setCredentials(credentials).setProjectId(cfg.projectName()).build().service
 
     override fun uploadUrl(objectName: String?): Result<URL?> = runCatching {
+        signURL(objectName, HttpMethod.PUT)
+    }
+
+    private fun signURL(objectName: String?, method: HttpMethod): URL? {
         val blobInfo = BlobInfo.newBuilder(BlobId.of(cfg.bucketName(), objectName)).build()
         val urlDuration = cfg.urlDuration()
         checkNotNull(urlDuration) { "could not read duration from config" }
-        storage.signUrl(
+        return storage.signUrl(
             blobInfo, urlDuration, TimeUnit.MINUTES,
-            Storage.SignUrlOption.httpMethod(HttpMethod.PUT),
+            Storage.SignUrlOption.httpMethod(method),
             Storage.SignUrlOption.withV4Signature()
         )
+    }
+
+    override fun downloadUrl(objectName: String?): Result<URL?> = runCatching {
+        signURL(objectName, HttpMethod.GET)
     }
 }
