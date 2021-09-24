8c62788f7fd796e9bfeed9561ce2a1df93e79904
jaecisneros <jcisneros@fbra.utn.edu.ar>
2021-09-23 22:56:14 -0300

    Updated project id

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 set -x
-PROJECT_ID=${PROJECT_ID:-"muebles-ra-300d"}
+PROJECT_ID=${PROJECT_ID:-"muebles-ra-a108"}
 REGION=${REGION:-"us-central1"}
 echo "Uploading project $PROJECT_ID"
 gcloud config set project $PROJECT_ID
@@ -10,4 +10,4 @@ gcloud builds submit --config cloudbuild.yaml . # --tag gcr.io/$PROJECT_ID/furni
 echo "Deploying with cloud-run"
 gcloud run deploy furniture-repository --image gcr.io/$PROJECT_ID/furniture-repository \
 --service-account=muebles-ra-sa@$PROJECT_ID.iam.gserviceaccount.com \
---memory 512M --no-allow-unauthenticated --region $REGION
+--memory 512M --allow-unauthenticated --region $REGION
