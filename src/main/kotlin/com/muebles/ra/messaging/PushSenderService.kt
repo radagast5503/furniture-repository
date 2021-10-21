4c7e6f4746b12c87c7af3d3e50a6468ffe2f08e6
jaecisneros <jcisneros@fbra.utn.edu.ar>
2021-10-21 00:57:25 -0300

    Added logs

--- src/main/kotlin/com/muebles/ra/messaging/PushSenderService.kt
+++ src/main/kotlin/com/muebles/ra/messaging/PushSenderService.kt
@@ -3,10 +3,13 @@ package com.muebles.ra.messaging
 import com.google.firebase.messaging.*
 import com.muebles.ra.api.NotificationStatus
 import com.muebles.ra.api.PubSubNotification
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Component
 
 @Component
 class PushSenderService {
+
+    private val logger = LoggerFactory.getLogger(javaClass)
 
     fun sendNotificationToPhone(notification: PubSubNotification) {
         val message: Message = Message.builder()
@@ -18,15 +21,6 @@ class PushSenderService {
         // registration token.
         val response = FirebaseMessaging.getInstance().send(message)
         // Response is a message ID string.
-        println("Successfully sent message: $response")
-    }
-
-    private fun title(status: String) : String {
-        return if (NotificationStatus.SUCCESS.statusName == status)  "Tu mueble esta listo!" else "Tuvimos un problema :("
-    }
-
-    private fun body(status: String) : String {
-        return if (NotificationStatus.SUCCESS.statusName == status)  "Baja tu mueble para empezar a usarlo."
-                else "Hubo un problema al virtualizar tu mueble. Por favor intentalo de nuevo."
+        logger.info("Successfully sent message: $response")
     }
 }
